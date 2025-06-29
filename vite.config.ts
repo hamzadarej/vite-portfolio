f3c7fdab7a88a479a4e9ff0f1b2626d4dc6368be
Hamza <hamza.darej@check24.de>
2025-06-29 16:42:32 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL || "/vite-portfolio",
+  base: process.env.VITE_BASE_PATH || "/vite-portfolio",
 });
